4d543659c2d3046a30d1d7d13a00a40d40fc73c9
Prashul Jain <jainprashul@gmail.com>
2024-02-09 04:06:02 -0500

    Add path variable for UI build directory

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import express from 'express'
+import * as path from 'path'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import apiRouter from './api.js'
 const app = express()
 const port = 80
 
+const uiPath = '/home/X/ui-server/ui/build'
+
+
 app.use(bodyParser.json())
 app.use(cors())
 app.use('/api', apiRouter)
-app.use(express.static('./ui/build'))
+app.use(express.static(uiPath))
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-    res.sendFile('./ui/build', 'index.html')
+    res.sendFile(uiPath, 'index.html')
 })
 
 // Start the server
